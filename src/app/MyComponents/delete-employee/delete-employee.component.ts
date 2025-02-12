04e7e076bdc9721ab2da6f15337deb321f14162f
Sanidhya Gupta <sanidhyagupta162002@gmail.com>
2025-02-13 03:06:49 +0530

    Generated the components

---
+++ src/app/MyComponents/delete-employee/delete-employee.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-employee',
+  imports: [],
+  templateUrl: './delete-employee.component.html',
+  styleUrl: './delete-employee.component.css'
+})
+export class DeleteEmployeeComponent {
+
+}
